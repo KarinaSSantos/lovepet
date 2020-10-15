eb598e5b994e58d2106bfa20743a4cbabf909ee8
3devweb <email@3dev.com.br>
2020-10-15 17:14:12 -0300

    Criando section sobre

--- src/home.php
+++ src/home.php
@@ -1,29 +1,49 @@
 <?php require 'inc/header.php'?>
 
-    <div class="Page_Content Home_Page">
-        <div class="Superbanner">
-                <div class="Content">
-                    <div class="Title">
+<div class="Page_Content Home_Page">
+    <div class="Superbanner">
+        <div class="Content">
+            <div class="Title">
                 <h1>Conheça nossa Iniciativa</h1>
-</div>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a consequat purus, facilisis iaculis nulla. Mauris vel leo bibendum, imperdiet justo a, pharetra metus.
-                </p>
-                <div class="Banner_Button">
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a consequat purus, facilisis iaculis
+                nulla. Mauris vel leo bibendum, imperdiet justo a, pharetra metus.
+            </p>
+            <div class="Banner_Button">
                 <div class="Primary_Button Solid">
                     <button>
                         <div class="Content">
                             Conhecer
                         </div>
-                        </button>
+                    </button>
                 </div>
-</div>
-</div>
-
-
             </div>
-
         </div>
+
+
+    </div>
+
+    <section class="AboutUs">
+        <div class="AboutUs_Content">
+            <div class="AboutUs_ContainerLeft">
+                <img src="http://localhost/lovepet/dist/img/SVG/quemsomos.svg">
+                </div>
+                <div class="AboutUs_ContainerRight">
+                <div class="AboutUs_Title">
+                <span> Sobre a nossa iniciativa</span>
+                <h2>Não compre! <br>
+                    <strong>Adote.</strong>
+                </h2>
+            </div>
+                <p>  Seu melhor amigo te espera! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a consequat purus, facilisis iaculis nulla. Mauris vel leo bibendum, imperdiet justo a, pharetra metus.
+                </p>
+</div>
+            
+        </div>
+    </section>
+
+</div>
 
 </div>
 
