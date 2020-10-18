16b5c7d6b11fdb4f7d0261ff8b3bdb1bb11365ca
3devweb <email@3dev.com.br>
2020-10-18 08:18:26 -0300

    section sobre abandono criada

--- src/home.php
+++ src/home.php
@@ -28,20 +28,49 @@
         <div class="AboutUs_Content">
             <div class="AboutUs_ContainerLeft">
                 <img src="http://localhost/lovepet/dist/img/SVG/quemsomos.svg">
-                </div>
-                <div class="AboutUs_ContainerRight">
-                <div class="AboutUs_Title">
-                <span> Sobre a nossa iniciativa</span>
-                <h2>Não compre! <br>
-                    <strong>Adote.</strong>
-                </h2>
             </div>
-                <p>  Seu melhor amigo te espera! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a consequat purus, facilisis iaculis nulla. Mauris vel leo bibendum, imperdiet justo a, pharetra metus.
+            <div class="AboutUs_ContainerRight">
+                <div class="AboutUs_Title">
+                    <span> Sobre a nossa iniciativa</span>
+                    <h2>Não compre! <br>
+                        <strong>Adote.</strong>
+                    </h2>
+                </div>
+                <p> Seu melhor amigo te espera! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a
+                    consequat purus, facilisis iaculis nulla. Mauris vel leo bibendum, imperdiet justo a, pharetra
+                    metus.
                 </p>
-</div>
-            
+            </div>
+
         </div>
     </section>
+    <section class="AbandonPets">
+        <div class="AbandonPets_Content">
+            <div class="AbandonPets_Container">
+                <div class="AboutUs_Title">
+                    <span> Sobre o abandono que combatemos</span>
+                    <h2>Todos os dias dezenas de animais são abandonados <br>
+                        <strong>Seja você uma fonte de esperança <span>adotando!</span></strong>
+                    </h2>
+                </div>
+
+                <div class="Banner_Button">
+                    <div class="Primary_Button Solid">
+                        <button>
+                            <div class="Content">
+                                Adotar
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="PetGallery">
+        
+</section>
 
 </div>
 
