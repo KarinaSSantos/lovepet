08cd07737c11e183111deb0e43c29c1f989dc486
3devweb <email@3dev.com.br>
2020-10-18 09:52:31 -0300

    criando galeria dos pets

--- src/home.php
+++ src/home.php
@@ -2,7 +2,7 @@
 
 <div class="Page_Content Home_Page">
     <div class="Superbanner">
-        <div class="Content">
+        <div class="SuperbannerContent">
             <div class="Title">
                 <h1>Conheça nossa Iniciativa</h1>
             </div>
@@ -14,7 +14,7 @@
                 <div class="Primary_Button Solid">
                     <button>
                         <div class="Content">
-                            Conhecer
+                            Conhecer <i class="fas fa-long-arrow-alt-right"></i>
                         </div>
                     </button>
                 </div>
@@ -47,7 +47,7 @@
     <section class="AbandonPets">
         <div class="AbandonPets_Content">
             <div class="AbandonPets_Container">
-                <div class="AboutUs_Title">
+                <div class="AbandonPets_Title">
                     <span> Sobre o abandono que combatemos</span>
                     <h2>Todos os dias dezenas de animais são abandonados <br>
                         <strong>Seja você uma fonte de esperança <span>adotando!</span></strong>
@@ -58,7 +58,7 @@
                     <div class="Primary_Button Solid">
                         <button>
                             <div class="Content">
-                                Adotar
+                                Adotar <i class="fas fa-long-arrow-alt-right"></i>
                             </div>
                         </button>
                     </div>
@@ -69,7 +69,99 @@
     </section>
 
     <section class="PetGallery">
-        
+        <div class="PetGallery_Content">
+            <div class="PetGallery_Container">
+                <div class="PetGallery_Title">
+                    Conheça os bichinhos
+                </div>
+                <div class="PetGalleryBox_Title">
+                    Pets disponíveis
+                </div>
+                <div class="PetGalleryBox __is-grid-3">
+                    <div class="PetGalleryBox_SingleItem">
+                        <div class="SingleItemPhoto">
+                            <img src="http://localhost/lovepet/dist/img/milla.jpg">
+                            <div class="SingleItemLocation">
+                                <span>Queimados - RJ </span>
+                            </div>
+                        </div>
+                        <div class="SingleItemContent">
+                            <div class="SingleItemName">
+                                <h3>Milla - 8 meses </h3>
+                            </div>
+                            <div class="SingleItemAbout">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a consequat purus,
+                                    facilisis iaculis nulla. Mauris vel leo bibendum.</p>
+                            </div>
+                            <div class="SingleItemDetails">
+                                <div class="Primary_Button Solid">
+                                    <button>
+                                        <div class="Content">
+                                            Conhecer <i class="fas fa-long-arrow-alt-right"></i>
+                                        </div>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="PetGalleryBox_SingleItem">
+                        <div class="SingleItemPhoto">
+                            <img src="http://localhost/lovepet/dist/img/jack.jpg">
+                            <div class="SingleItemLocation">
+                                <span>Queimados - RJ </span>
+                            </div>
+                        </div>
+                        <div class="SingleItemContent">
+                            <div class="SingleItemName">
+                                <h3>Jack - 4 anos</h3>
+                            </div>
+                            <div class="SingleItemAbout">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a consequat purus,
+                                    facilisis iaculis nulla. Mauris vel leo bibendum.</p>
+                            </div>
+                            <div class="SingleItemDetails">
+                                <div class="Primary_Button Solid">
+                                    <button>
+                                        <div class="Content">
+                                            Conhecer <i class="fas fa-long-arrow-alt-right"></i>
+                                        </div>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="PetGalleryBox_SingleItem">
+                        <div class="SingleItemPhoto">
+                            <img src="http://localhost/lovepet/dist/img/rogério.jpg">
+                            <div class="SingleItemLocation">
+                                <span>Queimados - RJ </span>
+                            </div>
+                        </div>
+                        <div class="SingleItemContent">
+                            <div class="SingleItemName">
+                                <h3>Rogério - 1 ano</h3>
+                            </div>
+                            <div class="SingleItemAbout">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a consequat purus,
+                                    facilisis iaculis nulla. Mauris vel leo bibendum.</p>
+                            </div>
+                            <div class="SingleItemDetails">
+                                <div class="Primary_Button Solid">
+                                    <button>
+                                        <div class="Content">
+                                            Conhecer <i class="fas fa-long-arrow-alt-right"></i>
+                                        </div>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
 </section>
 
 </div>
--- src/inc/head.php
+++ src/inc/head.php
@@ -15,7 +15,7 @@
         <meta property="og:locale" content="pt-br">
         <meta property="og:site_name" content="">
 
-        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
         <link rel="stylesheet" type="text/css" href="http://localhost/lovepet/dist/css/site.css">
         <script type="text/javascript" src="http://localhost/lovepet/dist/js/app.js"></script>
